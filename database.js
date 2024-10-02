4d25f4f6978b9fe0e23d4173d3d6595532049d7b
karan <karansingh36752@gmail.com>
2024-10-02 21:22:34 +0530

    DB

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient,ObjectId } = require('mongodb');
 
 const url = 
    "mongodb+srv://karansingh36752:dw0nWOUqPSk9n8aJ@nodejsnamaste.xfmxb.mongodb.net/"
@@ -12,11 +12,32 @@ async function main() {
     console.log('Connected successfully to server');
     const db = client.db(dbName);
     const collection = db.collection('User');
+
+    const data = {
+        "first_name" : "Gaurang",
+        "last_name" : "shandily",
+        "age" : 20,
+        "mobile_no" : "4567398423",
+        "gender" : "Male",
+
+    }
+
+   // Inserting the data
+    // const result = await collection.insertOne(data)
+    // console.log("the inserted Data =>" ,result);
   
-    // the following code examples can be pasted here...
+    //Update the data
+    // const updateResult = await collection.updateOne({_id: new ObjectId('66fcaf4acc3b29faeadbe273')},{$set: {first_name : "Aryan" , last_name : "Sharma"}});
+    // console.log(updateResult)
+
+    //Delete operation
+    const deleteResult = await collection.deleteOne({first_name:"Aman"});
+    console.log(deleteResult);
+  
     const findResult = await collection.find({}).toArray();
     console.log('Found documents =>', findResult);
   
+
     return 'done.';
   }
   
